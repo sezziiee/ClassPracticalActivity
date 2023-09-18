269b6edf7815aeaa48392e72412214ab6da9ff45
BradsBrother <104351035+BradsBrother@users.noreply.github.com>
2023-09-18 12:38:56 +0000

    added register button

--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -76,14 +76,5 @@ namespace CLDVClassGroupActivity
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogin;
-
-        /// <summary>
-        /// lblWarning control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblWarning;
     }
 }
