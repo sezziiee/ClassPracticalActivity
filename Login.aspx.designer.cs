a2addaeb7e890a5d9062e7fc921c8240772c5cc1
BradsBrother <104351035+BradsBrother@users.noreply.github.com>
2023-09-18 14:10:29 +0000

    fixed the login and styled the home page

--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -76,5 +76,14 @@ namespace CLDVClassGroupActivity
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogin;
+
+        /// <summary>
+        /// lblWarning control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblWarning;
     }
 }
